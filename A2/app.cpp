e2e2ba6a972be30fcbdc3239cadc86b2c1635a7a
Winson0117 <winsonng01@gmail.com>
2026-08-17 17:40:41 +0800

    Add project files.

---
+++ A2/app.cpp
@@ -0,0 +1,19 @@
+#include	<iostream>
+#include	<fstream>
+#include	<cstdlib>
+#include	<cstdio>
+#include	<ctime>
+#include	"BST.h"
+#include    "Student.h"
+
+using namespace std;
+
+bool readFile(const char *, BST *);
+int menu();
+
+int main() {
+
+	
+	system("pause");
+	return 0;
+}
---
+++ A2/readme.txt
@@ -0,0 +1,18 @@
+Group Member 1:
+
+Name: 
+Student ID:
+Course: (e.g.: CS)
+Practical Session: (e.g.: Monday, 11AM - 1PM)
+Practical Group: (e.g.: P1)
+Tutor: 
+
+
+Group Member 2:
+
+Name: 
+Student ID:
+Course:
+Practical Session: 
+Tutor:
+
